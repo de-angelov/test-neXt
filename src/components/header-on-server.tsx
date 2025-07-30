71d3dc0b752f6030fa7cfc1948dd937d11e02329
Denis Angelov <denis.angelov@dataart.com>
2025-07-30 09:05:35 +0300

    header style

--- src/components/header-on-server.tsx
+++ src/components/header-on-server.tsx
@@ -4,13 +4,14 @@ import "server-only";
 const tw = {
   container: "relative p-5 bg-cyan-200 flex flex-col min-h-5 w-full drop-shadow-md drop-shadow-red-950 z-40",
   nav: "flex min-h-5 w-full",
-  button: "bg-cyan-300 border rounded-sm inline-block p-2 m-1 hover:inner hover:bg-white drop-shadow-md drop-shadow-gray-500"
+  button: "bg-cyan-300 border rounded-sm inline-block p-2 m-1 hover:inner hover:bg-white drop-shadow-md drop-shadow-gray-500",
+  title: "underline decoration-wavy"
 };
 
 export const Nav = () => {
   return (
     <div className={tw.container}> 
-      <span>Runs on Server Only</span>
+      <span className={tw.title}>Runs on Server Only</span>
       <nav className={tw.nav}>
         <Link className={tw.button} href="/action-random">Action - Random</Link>
         <Link className={tw.button} href="/dynamic-random">Dynamic - Random</Link>
